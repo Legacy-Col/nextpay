da71574075c20160c8a698bd78dc7255e2f72f6a
Legacy-Col <lynxlestley@gmail.com>
2025-03-27 14:04:14 +0100

    message

---
+++ app/(api)/(validator)/Jwt.tsx
@@ -0,0 +1,29 @@
+import * as Jwt from "jsonwebtoken";
+import * as bcrypt from "bcrypt";
+
+
+const SECRET_KEY = "JWT_SECRET_KEY"
+
+export const hashPassword = async (password: string) => {
+    return await bcrypt.hash(password, 20);
+};
+
+export const comparePassword = async (password: string, hash: string) => {
+    return await bcrypt.compare(password, hash);
+};
+
+export const generateAccessToken = (user: { email: string }) => {
+    return Jwt.sign(user, SECRET_KEY, {expiresIn: "15min"})
+}
+
+export const generatRefreshToken = (user: { email: string }) => {
+    return Jwt.sign(user, SECRET_KEY, {expiresIn: "30d"})
+}
+
+export const verifyToken = (token: string) => {
+    try {
+        return Jwt.verify(token, SECRET_KEY)
+    } catch (error) {
+        return null
+    }
+}
